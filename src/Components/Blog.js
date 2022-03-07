970f24f3cfd8511d647a2e4814791860bf13950a
sainath <sainathpoojari8@gmail.com>
2022-03-07 21:47:33 +0530

    date time author and the source commit

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -4,17 +4,17 @@ import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 
 export class Blog extends Component {
-  
-    static defaultProps={
-      country:'in',
-      pageSize:6,
-      category:'genral'
-  }
-    static propTypes={
-      country:PropTypes.string,
-      pageSize:PropTypes.number,
-      category:PropTypes.string,
-  }
+
+    static defaultProps = {
+        country: 'in',
+        pageSize: 6,
+        category: 'genral'
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+    }
 
 
 
@@ -29,13 +29,13 @@ export class Blog extends Component {
 
     async componentDidMount() {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dce260f953ec48ffa94717ca5ae98d20&pageSize=${this.props.pageSize}`
-        this.setState({loading:true})
+        this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
-             loading: false
+            loading: false
         })
     }
 
@@ -47,46 +47,52 @@ export class Blog extends Component {
         this.setState({
             page: this.state.page - 1,
             articles: parsedData.articles,
-            loading:false
-            
+            loading: false
+
         })
     }
 
     handleNextclick = async () => {
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-                let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dce260f953ec48ffa94717ca5ae98d20&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
-                this.setState({ loading: true })
-                let data = await fetch(url);
-                let parsedData = await data.json()
-                this.setState({
-                    page: this.state.page + 1,
-                    articles: parsedData.articles,
-                    loading:false
-                })
-            }
-        }
-
-        render() {
-            return (
-                <>
-                    <div className='container my-3'>
-                        <h2 className='text-center'>BLOG - The News </h2>
-                        {this.state.loading && <Spinner />}
-                        <div className='row'>
-                            {!this.state.loading&&this.state.articles.map((element) => {
-                                return <div className='col-md-4' key={element.url}>
-                                    <BlogItem title={element.title ? element.title.slice(0, 35) : ""} description={element.description ? element.description.slice(0, 87) : ""} imgUrl={element.urlToImage} newsUrl={element.url} />
-                                </div>
-                            })}
-                            <div className='container d-flex justify-content-between my-3'>
-                                <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePreviousclick}> &larr; Previous</button>
-                                <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextclick}>Next &rarr; </button>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            )
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dce260f953ec48ffa94717ca5ae98d20&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
+            this.setState({ loading: true })
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles,
+                loading: false
+            })
         }
     }
+
+    render() {
+        return (
+            <>
+                <div className='container my-3'>
+                    <h2 className='text-center'>BLOG - The News </h2>
+                    {this.state.loading && <Spinner />}
+                    <div className='row'>
+                        {!this.state.loading && this.state.articles.map((element) => {
+                            return <div className='col-md-4' key={element.url}>
+                                <BlogItem title={element.title ? element.title.slice(0, 35) : ""}
+                                    description={element.description ? element.description.slice(0, 87) : ""}
+                                    imgUrl={element.urlToImage}
+                                    newsUrl={element.url}
+                                    author={element.author ? element.author : "unkown"}
+                                    date={element.publishedAt}
+                                    source={element.source.name} />
+                            </div>
+                        })}
+                        <div className='container d-flex justify-content-between my-3'>
+                            <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePreviousclick}> &larr; Previous</button>
+                            <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextclick}>Next &rarr; </button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
 
 export default Blog;
--- src/Components/BlogItem.js
+++ src/Components/BlogItem.js
@@ -3,15 +3,18 @@ import React, { Component } from 'react'
 
 export class BlogItem extends Component {
     render() {
-        let { title, description, imgUrl, newsUrl } = this.props
+        let { title, description, imgUrl, newsUrl,date,author,source } = this.props
         return (
             <div className='container '>
                 <div className="card my-4">
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    {source}</span>
                     <img src={imgUrl ? imgUrl : "https://i.insider.com/62232c8a99086300199890c7?width=1200&format=jpeg"}
                         className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
+                        <p> <small className="text-muted"> <strong>By {author} on {new Date(date).toGMTString()}</strong></small></p>
                         <a href={newsUrl} target="_blank" rel='noreferrer' className="btn  btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
